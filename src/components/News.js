1551d498ca9a2cd882c21b95c2adae731acc66c5
sharvilprabhudesai15 <sharvilprabhudesai15@gmail.com>
2023-05-21 18:37:05 +0530

    sixth commit latest 21/5/2023

--- src/components/News.js
+++ src/components/News.js
@@ -34,7 +34,7 @@ const News =(props)=>{
   }
 
   useEffect(() => {
-    document.title=`${capitalizeFirstLetter(props.category)}-NewsMonkey`;
+    document.title=`${capitalizeFirstLetter(props.category)}-IndiaNews`;
      updateNews();
      // eslint-disable-next-line
   },[] )
@@ -50,7 +50,7 @@ const News =(props)=>{
      };
     return (
       <>
-        <h1 className="text-center" style={{margin:'35px 0px',marginTop:'90px'}}>NewsMonkey - Top <strong>{capitalizeFirstLetter(props.category)} Category </strong>  Headlines </h1>
+        <h1 className="text-center" style={{margin:'35px 0px',marginTop:'90px'}}>IndiaNews - Top <strong>{capitalizeFirstLetter(props.category)} Category </strong>  Headlines </h1>
         {loading && <Spinner/>}
 
         <InfiniteScroll
